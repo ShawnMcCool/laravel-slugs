3b8d0e3ab887e08892cbe04454d1247612829d87
Shawn McCool <shawn@heybigname.com>
2013-09-21 16:50:42 +0200

    change the way empty strings are detected

--- src/McCool/LaravelSlugs/LaravelSlugsServiceProvider.php
+++ src/McCool/LaravelSlugs/LaravelSlugsServiceProvider.php
@@ -10,14 +10,14 @@ class LaravelSlugsServiceProvider extends ServiceProvider
         $this->package('mccool/laravel-slugs');
 
         Event::listen('eloquent.created: *', function($model, $event) {
-            if ($model instanceOf SlugInterface and $model->getSlugString() != '') {
+            if ($model instanceOf SlugInterface and strlen($model->getSlugString()) > 0) {
             	$generator = new SlugGenerator($model);
             	$generator->updateSlug();
             }
         });
 
         Event::listen('eloquent.updated: *', function($model, $event) {
-            if ($model instanceOf SlugInterface and $model->getSlugString() != '') {
+            if ($model instanceOf SlugInterface and strlen($model->getSlugString()) > 0) {
             	$generator = new SlugGenerator($model);
             	$generator->updateSlug();
             }
